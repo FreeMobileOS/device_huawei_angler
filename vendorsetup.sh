5e20f5f5b4a2b7a3c6f967ec711993ae1f395a35
Bernhard Rosenkränzer <bero@lindev.ch>
2018-04-09 15:19:57 +0200

    Use current preview blobs for 9.0.0
    Signed-off-by: Bernhard Rosenkränzer <bero@lindev.ch>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -15,8 +15,8 @@
 #
 
 if ! [ -d vendor/huawei/angler ]; then
-	[ -e huawei-angler-opm5.171019.015-a4ae0930.tgz ] || wget https://dl.google.com/dl/android/aosp/huawei-angler-opm5.171019.015-a4ae0930.tgz
-	[ -e qcom-angler-opm5.171019.015-f000ebde.tgz ] || wget https://dl.google.com/dl/android/aosp/qcom-angler-opm5.171019.015-f000ebde.tgz
+	[ -e huawei-angler-4654575-dbc76ebf.tgz ] || wget https://dl.google.com/dl/android/aosp/huawei-angler-4654575-dbc76ebf.tgz
+	[ -e qcom-angler-4654575-8c02d11f.tgz ] || wget https://dl.google.com/dl/android/aosp/qcom-angler-4654575-8c02d11f.tgz
 	for i in *-angler-*.tgz; do
 		tar xf $i
 	done
